e1f26b2ef9f8ffe30d31f123f413637115218e1f
Rishi Khan <rishi.rkhan@gmail.com>
2019-01-09 12:23:59 -0500

    Added date check for days to break even
    Compares today's date to date of expiry

--- cost_analaysis.py
+++ cost_analaysis.py
@@ -1,4 +1,6 @@
 import math as m
+from datetime import date
+import datetime
 
 def calc(cost, costPU):
     return m.ceil(cost/costPU)
@@ -7,4 +9,17 @@ print ("What is the cost of the item?")
 cost = float(input())
 print ("What is the normal cost per use of the item?")
 costPU = float(input())
-print ("You will break even in {} uses".format(calc(cost,costPU)))
+totalUses = calc(cost,costPU)
+print ("Does this item have a time limit?[y/n]")
+answer = input()
+if answer == 'y' or answer == 'Y':
+    print ("What date will the item expire?")
+    date_entry = input('Enter a date in YYYY-MM-DD format: ')
+    year, month, day = map(int, date_entry.split('-'))
+    date1 = datetime.date(year, month, day)
+    now = date.today()
+    days = (date1 - now).days
+    print ("You will break even in {} uses".format(totalUses))
+    print ("You will need to use {:.1f} per day for {} days".format(totalUses/days, days))
+else:
+    print ("You will break even in {} uses".format(totalUses))
